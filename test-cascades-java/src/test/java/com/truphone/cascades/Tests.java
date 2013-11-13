3eab5b1a7768f78b7e34dba5186aa9ccfe1bc0c3
Sam Truscott <samuel.truscott@truphone.com>
2013-11-13 15:17:57 +0000

    Added an internal fake server

--- test-cascades-java/src/test/java/com/truphone/cascades/Tests.java
+++ test-cascades-java/src/test/java/com/truphone/cascades/Tests.java
@@ -1,13 +1,21 @@
 package com.truphone.cascades;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Semaphore;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.truphone.cascades.SynchronousConnection.TimeoutException;
-import com.truphone.cascades.commands.TextCommand;
 import com.truphone.cascades.commands.ClickCommand;
 import com.truphone.cascades.commands.SleepCommand;
+import com.truphone.cascades.commands.TextCommand;
 import com.truphone.cascades.commands.ToastCommand;
 import com.truphone.cascades.replys.IReply;
 
@@ -16,19 +24,18 @@ import com.truphone.cascades.replys.IReply;
  */
 public final class Tests {
 
-	private String deviceIpAddress;
-	private int deviceIpPort;
-	private final int defaultTimeoutPeriod = 1000;
-	private final int defaultSleepPeriod = 3000;
+	private FakeDevice device;
+	private static final int DEFAULT_TIMEOUT_PERIOD = 1000;
+	private static final int DEFAULT_SLEEP_PERIOD = 3000;
 	private SynchronousConnection conn;
 	
 	@Before
-    public void setUp() {
-        deviceIpAddress = System.getProperty("deviceIpAddress", "192.168.70.130");
-        deviceIpPort = Integer.parseInt(System.getProperty("deviceIpPort", "15000"));
-        conn = new SynchronousConnection(deviceIpAddress, deviceIpPort);
+	public void testInit() {
+		device = new FakeDevice(15000);
+		device.listen();
+        conn = new SynchronousConnection("localhost", 15000);
         try {
-        	conn.connect(defaultTimeoutPeriod);
+        	conn.connect(DEFAULT_TIMEOUT_PERIOD);
         } catch(TimeoutException exception) {
         	Assert.fail(exception.getLocalizedMessage());
         }
@@ -40,7 +47,7 @@ public final class Tests {
 				conn.transmit(new TextCommand(
 						"myUsernameField",
 						"my.user-name"),
-						defaultTimeoutPeriod);
+						DEFAULT_TIMEOUT_PERIOD);
 		Assert.assertTrue(reply.isSuccess());
 	}
 	
@@ -50,7 +57,7 @@ public final class Tests {
 		 final IReply reply = 
 				 conn.transmit(new ClickCommand(
 						 "theLoginButton"),
-						 defaultTimeoutPeriod);
+						 DEFAULT_TIMEOUT_PERIOD);
 		 Assert.assertTrue(reply.isSuccess());
 	 }
 	 
@@ -59,7 +66,7 @@ public final class Tests {
 		 final IReply reply = 
 				 conn.transmit(new ToastCommand(
 						 "You didn't enter a password"),
-						 defaultTimeoutPeriod);
+						 DEFAULT_TIMEOUT_PERIOD);
 		 Assert.assertTrue(reply.isSuccess());
 	 }
 	 
@@ -67,8 +74,107 @@ public final class Tests {
 	 public void testSleepCommand() throws TimeoutException {
 		 final IReply reply = 
 				 conn.transmit(new SleepCommand(
-						 defaultSleepPeriod),
-						 defaultTimeoutPeriod);
+						 DEFAULT_SLEEP_PERIOD),
+						 DEFAULT_TIMEOUT_PERIOD);
 		 Assert.assertTrue(reply.isSuccess());
+	 }
+	 
+	 @After
+	 public void testExit() {
+		 device.stop();
+	 }
+	 
+	 private static final class FakeDevice {
+		 
+		 private static final class FakeDeviceProcess implements Runnable {
+			 
+			 private final ServerSocket server;
+			 private Socket client;
+			 private boolean running;
+			 
+			 public FakeDeviceProcess(final ServerSocket socket_) {
+				 this.server = socket_;
+				 this.running = true;
+			 }
+			 
+			 public void run() {
+				 try {
+					 while(running) {
+						 client = server.accept();
+						 client.getOutputStream().write("OK\n".getBytes());
+						 final BufferedReader reader = 
+								 new BufferedReader(
+										 new InputStreamReader(client.getInputStream()));
+						 String line = reader.readLine();
+						 while (line != null) {
+							 System.out.println(line);
+							 if (line.equals("record")) {
+								 client.getOutputStream().write("OK\n".getBytes());
+							 }
+						 }
+						 client = null;
+					 }
+				 } catch (Throwable t) {
+				 }
+			 }
+			 
+			 public void shutdown() {
+				 running = false;
+				 try {
+					 this.server.close();
+				 } catch(Throwable t) {}
+				 if (client != null) {
+					 try {
+						 client.close();
+					 }catch (Throwable t) {}
+				 }
+			 }
+		 }
+		 
+		 private final ServerSocket server;
+		 private final Semaphore lock;
+		 private boolean isListening;
+		 private FakeDeviceProcess fakeDeviceProcess;
+		 private Thread serverThread;
+		 
+		 public FakeDevice(final int port) {
+			 this.lock = new Semaphore(1);
+			 this.isListening = false;
+			 try {
+				 this.server = new ServerSocket(port);
+			 } catch(IOException ioe) {
+				 throw new RuntimeException("Can't create the fake device port - fail");
+			 }
+		 }
+		 
+		 public boolean listen() {
+			 boolean r = false;
+			 try {
+				 this.lock.acquire();
+				 if (!isListening) {
+					 serverThread = new Thread(
+							 fakeDeviceProcess = new FakeDeviceProcess(this.server));
+					 r = true;
+					 serverThread.start();
+					 isListening = true;
+				 }
+				 this.lock.release();
+			 } catch(Throwable t) {
+			 }
+			 return r;
+		 }
+		 
+		 public void stop() {
+			 try {
+				 this.lock.acquire();
+				 if (isListening) {
+					 this.fakeDeviceProcess.shutdown();
+					 this.serverThread.join();
+					 this.isListening = false;
+				 }
+				 this.lock.release();
+			 } catch(Throwable t) {
+			 }
+		 }
 	 }
 }
