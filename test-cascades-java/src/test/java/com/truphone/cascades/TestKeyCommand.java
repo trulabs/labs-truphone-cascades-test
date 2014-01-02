97e052cfd576dddbd5da3eaca50404eb5bf8ac3d
Sam Truscott <samuel.truscott@truphone.com>
2014-01-02 16:53:36 +0000

    Add unit tests for KeyCommand. Fix the KeyCommand.

---
+++ test-cascades-java/src/test/java/com/truphone/cascades/TestKeyCommand.java
@@ -0,0 +1,37 @@
+package com.truphone.cascades;
+
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.truphone.cascades.FakeDevice.FakeDeviceListener;
+import com.truphone.cascades.commands.KeyCommand;
+import com.truphone.cascades.replys.IReply;
+
+/**
+ * Test class.
+ * @author STruscott
+ *
+ */
+public final class TestKeyCommand {
+	/**
+	 * Test the key command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testKeyCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("key 65 1 1 1 1".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new KeyCommand('A', true, true, true, true), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
+}
