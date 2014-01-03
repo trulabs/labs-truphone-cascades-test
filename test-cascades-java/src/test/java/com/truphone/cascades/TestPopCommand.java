1f2c63c97b68c0d2346e25fde457a2b9d360078f
Sam Truscott <samuel.truscott@truphone.com>
2014-01-03 10:43:31 +0000

    Unit tests for the pop command.

---
+++ test-cascades-java/src/test/java/com/truphone/cascades/TestPopCommand.java
@@ -0,0 +1,39 @@
+package com.truphone.cascades;
+
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.truphone.cascades.FakeDevice.FakeDeviceListener;
+import com.truphone.cascades.commands.PopCommand;
+import com.truphone.cascades.replys.IReply;
+
+/**
+ * Test class.
+ * @author STruscott
+ *
+ */
+public final class TestPopCommand {
+	/**
+	 * Test the key command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testKeyCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("pop".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				} else {
+					Assert.fail(message);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new PopCommand(), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
+}
