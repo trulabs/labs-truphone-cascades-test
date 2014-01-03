1db77aaa8670968d24845124cc4bc0cb2d396ea2
Sam Truscott <samuel.truscott@truphone.com>
2014-01-03 11:01:37 +0000

    Unit test for the test command.

---
+++ test-cascades-java/src/test/java/com/truphone/cascades/TestTestCommand.java
@@ -0,0 +1,39 @@
+package com.truphone.cascades;
+
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.truphone.cascades.FakeDevice.FakeDeviceListener;
+import com.truphone.cascades.commands.TestCommand;
+import com.truphone.cascades.replys.IReply;
+
+/**
+ * Test class.
+ * @author STruscott
+ *
+ */
+public final class TestTestCommand {
+	/**
+	 * Test the Test command command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testPopCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("test obviousTextField text Susan Boyle is pretty".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				} else {
+					Assert.fail(message);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new TestCommand("obviousTextField", "text", "Susan Boyle is pretty"), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
+}
