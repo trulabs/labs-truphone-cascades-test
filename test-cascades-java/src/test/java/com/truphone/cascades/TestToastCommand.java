ea954db5061a527d37fce7676004479c926ac9d7
Sam Truscott <samuel.truscott@truphone.com>
2014-01-03 10:42:11 +0000

    Update the Toast unit tests.

--- test-cascades-java/src/test/java/com/truphone/cascades/TestToastCommand.java
+++ test-cascades-java/src/test/java/com/truphone/cascades/TestToastCommand.java
@@ -36,4 +36,48 @@ public final class TestToastCommand {
 		FakeDevice.DEVICE.getProcess().removeListener(response);
 		Assert.assertTrue(reply.isSuccess());
 	}
+
+	/**
+	 * Test the toast command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testToastOnCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("toast true".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				} else {
+					Assert.fail(message);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new ToastCommand(true), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
+
+	/**
+	 * Test the toast command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testToastHiddenCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("toast false".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				} else {
+					Assert.fail(message);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new ToastCommand(false), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
 }
