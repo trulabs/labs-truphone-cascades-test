9650498e87b976b9446e996fb5f98e053abee0a3
Sam Truscott <samuel.truscott@truphone.com>
2014-01-03 10:50:13 +0000

    Add unit test for the QML Command

---
+++ test-cascades-java/src/test/java/com/truphone/cascades/TestQmlCommand.java
@@ -0,0 +1,39 @@
+package com.truphone.cascades;
+
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.truphone.cascades.FakeDevice.FakeDeviceListener;
+import com.truphone.cascades.commands.QmlCommand;
+import com.truphone.cascades.replys.IReply;
+
+/**
+ * Test class.
+ * @author STruscott
+ *
+ */
+public final class TestQmlCommand {
+	/**
+	 * Test the key command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testKeyCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("qml appContext.function".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				} else {
+					Assert.fail(message);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new QmlCommand("appContext.function"), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
+}
