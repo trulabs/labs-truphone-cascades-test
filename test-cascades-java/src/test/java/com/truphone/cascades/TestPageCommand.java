3caf4c8fce7cd072c72f42f3ff442ec600720de2
Sam Truscott <samuel.truscott@truphone.com>
2014-01-03 10:40:53 +0000

    Units tests for the page command.

---
+++ test-cascades-java/src/test/java/com/truphone/cascades/TestPageCommand.java
@@ -0,0 +1,39 @@
+package com.truphone.cascades;
+
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.truphone.cascades.FakeDevice.FakeDeviceListener;
+import com.truphone.cascades.commands.PageCommand;
+import com.truphone.cascades.replys.IReply;
+
+/**
+ * Test class.
+ * @author STruscott
+ *
+ */
+public final class TestPageCommand {
+	/**
+	 * Test the key command.
+	 * @throws TimeoutException Thrown if the command times out
+	 */
+	@Test
+	public void testKeyCommand() throws TimeoutException {
+		final FakeDeviceListener response = new FakeDeviceListener() {
+			@Override
+			public void messageReceived(String message, PrintStream replyStream) {
+				if ("page somePage".equals(message)) {
+					replyStream.println(FakeDevice.OK_MESSAGE);
+				} else {
+					Assert.fail(message);
+				}
+			}
+		};
+		FakeDevice.DEVICE.getProcess().addListener(response);
+		final IReply reply = FakeDevice.CONN.transmit(new PageCommand("somePage"), FakeDevice.DEFAULT_TIMEOUT);
+		FakeDevice.DEVICE.getProcess().removeListener(response);
+		Assert.assertTrue(reply.isSuccess());
+	}
+}
